0d9b89313439ce949932c78b35d7322ce0be226c
Valerie Chang <valeriechang0@gmail.com>
2022-03-01 16:22:09 +0100

    started backend

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -2,8 +2,16 @@ class PagesController < ApplicationController
   skip_before_action :authenticate_user!, only: :home
 
   def home
-    # @me = RSpotify::User.find(current_user.uid)
+    @user_now = RSpotify::User.find(current_user.uid)
+    # title of first playlist - playlist_titles = @present_user.playlists.first.name
+
     # track_uid = @me.playlists.first.tracks.first.uri.split(':').last
     # @track = RSpotify::Track.find(track_uid)
+    raise
+  end
+
+  def profile
+    @user_now = RSpotify::User.find(current_user.uid)
+    @display_name = @user_now.display_name
   end
 end
---
+++ app/controllers/posts_controller.rb
@@ -0,0 +1,53 @@
+class PostsController < ApplicationController
+  def index
+    # authorize @post
+  end
+
+  def show
+    # authorize @post
+  end
+
+  def new
+    @post = Post.new
+    # authorize @post
+  end
+
+  def create
+    @user = current_user
+    @post = Post.new(post_params)
+    # authorize @post
+
+    @post.user = @user
+    if @post.save
+      redirect_to post_path(@post), notice: "Posted!"
+    else
+      render :new, alert: "Not posted!"
+    end
+  end
+
+  def edit
+    @post = Post.find(params[:id])
+    # authorize @post
+  end
+
+  def update
+    @post = Post.find(params[:id])
+    # authorize @post
+    @post.update(post_params)
+    redirect_to post_path(@post)
+  end
+
+  def destroy
+    @post = Post.find(params[:id])
+    # authorize @post
+
+    @post.destroy
+    redirect_to posts_path
+  end
+
+  private
+
+  def post_params
+    params.require(:post).permit(:spotify_url, :title, :content)
+  end
+end
