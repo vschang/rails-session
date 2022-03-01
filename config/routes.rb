7b89ced5fbbb1bc8054a4ac9a1a96b70f964a814
Valerie Chang <valeriechang0@gmail.com>
2022-03-01 16:42:38 +0100

    profile up

--- config/routes.rb
+++ config/routes.rb
@@ -5,4 +5,7 @@ Rails.application.routes.draw do
     resources :repost, only: [:create]
   end
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  get 'profile', to: 'pages#profile'
+  get 'landing_page', to: 'pages#landing_page'
+
 end
