d524c2ee3ae77b22459e2fc02d062b579b33b421
pablo barcena <pablobarcena99@gmail.com>
2022-03-01 18:05:46 +0100

    added first css

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -7,14 +7,16 @@ class PagesController < ApplicationController
 
     # track_uid = @me.playlists.first.tracks.first.uri.split(':').last
     # @track = RSpotify::Track.find(track_uid)
-  
+
   end
 
   def profile
+    @username = current_user.username
     @user_now = RSpotify::User.find(current_user.uid)
     @full_name = @user_now.display_name
     @all_playlists = @user_now.playlists
     @prof_pic_url = @user_now.images[0]["url"]
+    
   end
 
   def landing_page
