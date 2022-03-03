bf64f00b8564ae79f280f917e0a5ec704fbc3738
Valerie Chang <valeriechang0@gmail.com>
2022-03-03 10:35:31 +0100

    started friendship model and nearly finished  repost/post model

--- app/controllers/reposts_controller.rb
+++ app/controllers/reposts_controller.rb
@@ -1,5 +1,14 @@
 class RepostsController < ApplicationController
   before_action :set_post
+  before_action :get_user
+
+  def get_user
+    @username = current_user.username
+    @user_now = RSpotify::User.find(current_user.uid)
+    @full_name = @user_now.display_name
+    @all_playlists = @user_now.playlists
+    @prof_pic_url = @user_now.images[0]["url"]
+  end
 
   def create
     @user = current_user
@@ -21,6 +30,11 @@ class RepostsController < ApplicationController
     @repost.destroy
     redirect_to posts_path
   end
+
+  def show
+    @repost = Repost.find(params[:id])
+  end
+
 
   private
 
